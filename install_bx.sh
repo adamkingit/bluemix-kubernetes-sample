3c0b316a00cb27294de3e08d78de9f0db82e389b
Adam King <rak@us.ibm.com>
2017-06-05 14:23:35 -0400

    /tmp/kube
    Signed-off-by: Adam King <rak@us.ibm.com>

--- install_bx.sh
+++ install_bx.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 echo "Install kubectl"
-wget --quiet --output-document=/tmp/kubectl  https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
-chmod +x /tmp/kubectl
+mkdir /tmp/kube
+wget --quiet --output-document=/tmp/kube/kubectl  https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+chmod +x /tmp/kube/kubectl
 
-export PATH="/tmp/Bluemix_CLI/bin:$PATH"
+export PATH="/tmp/kube:$PATH"
